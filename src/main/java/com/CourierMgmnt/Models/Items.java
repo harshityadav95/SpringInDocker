b11a645c9fb6a72cbfcdaff7496bdcb69ebe2974
Harshit Yadav <harshit_yadav@optum.com>
2021-08-31 13:31:41 +0530

    Project Created

---
+++ src/main/java/com/CourierMgmnt/Models/Items.java
@@ -0,0 +1,8 @@
+package com.CourierMgmnt.Models;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Items {
+
+}
---
+++ src/main/java/com/CourierMgmnt/SpringInDockerApplication.java
@@ -0,0 +1,13 @@
+package com.CourierMgmnt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringInDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringInDockerApplication.class, args);
+	}
+
+}
